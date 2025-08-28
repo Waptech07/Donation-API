503d2905ca815f8119f4d9a790edccdcc9ed5047
Waptech07 <abdulfataiyakub07@gmail.com>
2025-08-28 10:14:07 +0100

    Updated security.js

--- utils/security.js
+++ utils/security.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { Wallet } = require('../models');
 
 exports.verifyTransactionPin = async (userId, transactionPin) => {
